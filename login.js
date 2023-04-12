3eaaddf0efb328ab4b0c852c7f392e28135b8c1b
vishwesh129 <nakkalavishwesh@gmail.com>
2023-04-12 21:13:01 +0530

    login page script

--- login.js
+++ login.js
@@ -1,45 +1,40 @@
-function slideshowBestSellers() {
-    let slide_img = document.getElementById('slide-img');
-    let img = document.createElement('img');
+// function slideshow() {
+//     let slide_img = document.getElementById('slide-img');
+//     let img = document.createElement('img');
 
-    let best_sellers_arr = ['https://www.1mg.com/images/login-signup/Your-Go-To-Health-App.png',
-        'https://www.1mg.com/images/login-signup/Know-Your-Medicines.png', 
-        'https://www.1mg.com/images/login-signup/Home-Delivery-of-Medicines.png', 
-        'https://www.1mg.com/images/login-signup/Lab-Tests-at-Home.png',
-        'https://www.1mg.com/images/login-signup/Health-Related-Queries.png'
-    ];
+//     let corousel_arr = ['https://www.1mg.com/images/login-signup/Your-Go-To-Health-App.png',
+//         'https://www.1mg.com/images/login-signup/Know-Your-Medicines.png', 
+//         'https://www.1mg.com/images/login-signup/Home-Delivery-of-Medicines.png', 
+//         'https://www.1mg.com/images/login-signup/Lab-Tests-at-Home.png',
+//         'https://www.1mg.com/images/login-signup/Health-Related-Queries.png'
+//     ];
 
-    img.src = best_sellers_arr[0];
-    slide_img.appendChild(img);
-    let i = 1;
-    setInterval(function () {
-        img.src = best_sellers_arr[i % best_sellers_arr.length];
-        // img.style.width = '100%';
-        i++;
-        slide_img.append(img);
-    }, 3000);
+//     img.src = corousel_arr[0];
+//     slide_img.appendChild(img);
+//     let i = 1;
+//     setInterval(function () {
+//         img.src = corousel_arr[i % corousel_arr];
+//         // img.style.width = '100%';
+//         i++;
+//         slide_img.append(img);
+//     }, 3000);
 
-}
-slideshowBestSellers();
+// }
+// slideshow();
 
+let storedUserData = JSON.parse(localStorage.getItem('userData'));
 
-var credentialsdata = JSON.parse(localStorage.getItem("credentials")) || []
-
-function loginBtn(){
-    var f = 0;
-
-    var email = document.querySelector("#email").value
-    var password = document.querySelector("#password").value
-
-    credentialsData.forEach(function(elem){
-        if (email == elem.email && password == elem.password) {
-            alert("login successfull");
-            window.location.assign("index.html");
-            f = 1
-            localStorage.setItem("username", ele.firstname);
-        }
-        else{
-            alert("Invalid Email and Password");
-        }
-    })
-}
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+  
+    if (storedUserData && email === storedUserData.email && password === storedUserData.password) {
+      alert("Login successful");
+      window.location.href = 'index.html';
+    } else {
+      alert('Invalid credentials');
+    }
+});
+  
--- signup.js
+++ signup.js
@@ -1,24 +1,24 @@
-var signupdata=JSON.parse(localStorage.getItem("credentials")) || []
-
-function signupBtn(){
-
-    var info={
-        email : document.querySelector("#email").value,
-        username : document.querySelector("#username").value,
-        password : document.querySelector("#password").value,
-    }
-
-    if (info.username != "" && info.email != "" && info.password != ""  ) {
+document.getElementById('signup-form').addEventListener('submit', signupBtn);
+  function signupBtn(){
+    event.preventDefault();
+    
+    const userData = {
+        email: document.getElementById('email').value,
+        name: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+    if (userData.email != "" && userData.name != "" && userData.password != "") {
         alert("Account Created Successfully, You will be redirected to login Page!")
     }
-
-    signupdata.push(info);
-
-    localStorage.setItem("credentials" , JSON.stringify(signupdata))
+    else{
+        alert("Please fill all credentials");
+    }
+    
+    localStorage.setItem('userData', JSON.stringify(userData));
 
     document.querySelector("#email").value=""
     document.querySelector("#username").value=""
     document.querySelector("#password").value=""
 
-    window.location.href="login.html"
-}
+    window.location.href = 'login.html';
+  };
