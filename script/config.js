9ca2aa02815283ceafe7d2b00346ad3ae5a14859
Prasad <prasadmhaske2001@gmail.com>
2023-07-15 23:54:51 +0530

    working on server

--- script/config.js
+++ script/config.js
@@ -1,2 +1,3 @@
 var API_URL_JSON = 'https://onemg-server-1muy.onrender.com'
-var API_URL = 'http://localhost:8000'
+// var API_URL = 'http://localhost:8000'
+var API_URL = 'https://onemg-server-1muy.onrender.com'
